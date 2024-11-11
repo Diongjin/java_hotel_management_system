490d4d1cefbe3e0f36a856e870ae7bc532407363
Diongjin <like2532@naver.com>
2024-11-11 18:10:24 +0900

    Initial commit

---
+++ src/main/java/cse/deu/hms/HMS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cse.deu.hms;
+
+/**
+ *
+ * @author SAMSUNG
+ */
+public class HMS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
